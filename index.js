a57b593e4557650c836a3d5b0f97e12d9ae7d430
noman2047 <nomancse101@gmail.com>
2022-10-27 16:23:07 +0600

    Course Details

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express();
+const cors=require('cors');
 const port = process.env.PORT || 5000;
 
-const allTopics = require('./')
+const allTopics = require('./Data/topicId.json')
+const allTopicsDetails = require('./Data/topicNameDetails.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{
   res.send('Topic List.');
 });
+
+app.get('/course-details',(req,res)=>{
+  res.send(allTopicsDetails);
+})
+
+app.get('/course-topic',(req,res)=>{
+  res.send(allTopics);
+})
 
 app.listen(port, ()=>{
   console.log('Topic details.',port);
